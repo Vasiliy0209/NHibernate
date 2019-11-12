2ad5a41acb7eb90966ccdc5ce0323810aedb1b1e
Vasiliy0209 <54793048+Vasiliy0209@users.noreply.github.com>
2019-11-12 13:37:54 +0500

    Add files via upload

---
+++ MyAppology/ImSorry/ImSorry/Objects/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ImSorry
+{
+    class Employee
+    {
+        public virtual Guid ID { get; set; }
+        public virtual string Imya { get; set; }
+        public virtual int Age { get; set; }
+    }
+}
---
+++ MyAppology/ImSorry/ImSorry/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace ImSorry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Configuration();
+            c.Configure();
+            c.AddAssembly("ImSorry");
+            new SchemaExport(c).Execute(true, true, false);
+
+            /* ISessionFactory _sessionFactory;
+             _sessionFactory = c.BuildSessionFactory();
+             ISession session = _sessionFactory.OpenSession();*/
+
+            ISession session=c.BuildSessionFactory().OpenSession();
+
+            var vasya = new Employee();
+            vasya.Imya = "Sami ponimaete vasya";
+            vasya.Age = 40;
+            session.Save(vasya);
+            session.Flush();
+
+            var kolya = new Employee();
+            kolya.Imya = "A eto Kolya";
+            kolya.Age = 10;
+            session.Save(kolya);
+            session.Flush();
+
+            //IList<Employee> list = session.CreateCriteria(typeof(Employee)).List<Employee>();
+            IList<Employee> list = session.QueryOver<Employee>().Where(x => x.Age > 5).List<Employee>();
+            foreach (Employee emp in list)
+                Console.WriteLine(emp.ID+" "+emp.Imya);
+
+
+            Console.ReadLine();
+        }
+    }
+}
